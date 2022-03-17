fb4ac234fa5fe553c1530561440b19156599d6f1
John Berberian <53021080+LindirQuenya@users.noreply.github.com>
2022-03-17 19:07:03 -0400

    Commented, added docker support, removed symlink, and removed accidental write. (#1)
    * Commented, added docker support, and removed an accidental write to /tmp/union.
    
    * Use #uzip suffix, and eliminate unnecessary symlink.
    
    * Make the comments fewer, and close the file *before* releasing the lock.

--- mount.php
+++ mount.php
@@ -1,53 +1,101 @@
 <?php
 // Do not use this outside of a trusted environment!
+// Gets the device name (sda1, vda2, etc.) from the serial number when in QEMU.
+// When in docker, returns the input if it exists in /mnt/docker.
 function find_device_by_serial($serial) {
-	$dir = opendir("/sys/block");
-	while (false !== ($entry = readdir($dir))) {
-		if ($entry == "." or $entry == "..") {
-			continue;
+	// Check: running in docker?
+	if (file_exists("/.dockerenv")) {
+		$dir = opendir("/mnt/docker");
+		while (false !== ($entry = readdir($dir))) {
+			if ($entry == "." or $entry == "..") {
+				continue;
+			}
+			// Check for a match with the filename (not serial, careful!).
+			if ($serial === $entry) {
+				closedir($dir);
+				return $entry;
+			}
 		}
-		$found = @file_get_contents("/sys/block/${entry}/serial");
-		if ($serial === $found) {
-			closedir($dir);
-			return $entry;
+		closedir($dir);
+	} else {
+		// No, search for it as a device.
+		$dir = opendir("/sys/block");
+		while (false !== ($entry = readdir($dir))) {
+			if ($entry == "." or $entry == "..") {
+				continue;
+			}
+			$found = @file_get_contents("/sys/block/${entry}/serial");
+			if ($serial === $found) {
+				closedir($dir);
+				return $entry;
+			}
 		}
+		closedir($dir);
 	}
-	closedir($dir);
 }
+// The device location is /dev/ in QEMU and /mnt/docker/ in docker.
+$devLocation = "/dev/";
+if (file_exists("/.dockerenv")) {
+	$devLocation = "/mnt/docker/";
+}
+
 $serial = $_GET["file"];
+
 $attempts = 100;
+
 $device = find_device_by_serial($serial);
+
 while (!$device and $attempts--) {
 	usleep(100000);
 	$device = find_device_by_serial($serial);
 }
+
 if (!$device) {
 	http_response_code(400);
 	die("NO_SUCH_FILE");
 }
 // Race possible here
+// The file /tmp/union tracks all the directories currently union-mounted.
 $union = file_get_contents("/tmp/union");
+// Check if the device identifier is already in the list of union-mounted directories.
+// Later on, when we union-mount directories, they will be of the form "/tmp/${device}/content".
+// Because of this, searching for the device id is sufficient to tell if it's already mounted.
 if (strpos($union, $device) !== false) {
 	die("ALREADY_MOUNTED");
 }
-error_log("Mounting /dev/${device}");
-exec("sudo ln -s /dev/${device} /tmp/${device}.zip");
+// Watch carefully: we're going to transparently unzip/mount that gamezip without *ever* touching persistent storage.
+// Okay, we're mounting it. Log that.
+error_log("Mounting ${devLocation}${device}");
+
 exec("mkdir /tmp/${device}");
-exec("sudo fuzzyfs /root/.avfs/tmp/${device}.zip# /tmp/${device} -o allow_other");
+// Funny thing about avfs: it lets you access the contents of zips transparantly.
+// "/root/.avfs" is where avfs is running.
+// "/root/.avfs/tmp/${device}#uzip" is the "folder" with the contents of that zip.
+// The #uzip extension tells avfs that it's a zip - avfs supports many other formats too.
+// Fuzzy-mounting is just a case-insensitive bind-mount.
+exec("sudo fuzzyfs /root/.avfs${devLocation}${device}#uzip /tmp/${device} -o allow_other");
+
 $content = "/tmp/${device}/content";
+
 if (!is_dir($content)) {
 	http_response_code(400);
 	die("NO_CONTENT_FOLDER");
 }
+
 $lock = fopen("/tmp/lock", "w+");
+// flock is blocking, so this isn't really an if.
 if (flock($lock, LOCK_EX)) {
+	// Get the latest contents of /tmp/union.
 	$union = file_get_contents("/tmp/union");
 	$union = "${content}:${union}";
+
 	exec("sudo umount -l /var/www/localhost/htdocs");
 	exec("sudo unionfs '/root/base:${union}' /var/www/localhost/htdocs -o allow_other");
+
 	file_put_contents("/tmp/union", $union);
-	flock($lock, LOCK_UN);
+
 	fclose($lock);
+	flock($lock, LOCK_UN);
 }
-file_put_contents("/tmp/union", $union);
+
 echo "OK";
